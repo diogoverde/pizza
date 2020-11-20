723fad77bceafffb931b0ed2a043849ac96531e2
Diogo Verde <diogo.verde05@gmail.com>
2020-11-20 12:51:05 +0000

    Added deserts part

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter as Router } from "react-router-dom";
 import { GlobalStyle } from "./globalStyles";
 import Hero from "./components/Hero";
 import Products from "./components/Products";
-import { productData } from "./components/Products/data";
+import { productData, desertData } from "./components/Products/data";
 import Feature from "./components/Feature";
 
 function App() {
@@ -13,6 +13,7 @@ function App() {
       <Hero />
       <Products heading="Choose your favourite" data={productData} />
       <Feature />
+      <Products heading="Sweet Treats for you!" data={desertData} />
     </Router>
   );
 }
